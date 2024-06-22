56a5a82886129d3894c6a860a917f33f5912da88
Δimitrios Follas <dimitriosfollas@outlook.com>
2024-06-22 12:34:34 +0200

    Ask user to confirm before closing the main window

--- views/start_window.py
+++ views/start_window.py
@@ -7,7 +7,7 @@ class LibraryApplication(tk.Tk):
     
     def __init__(self):
         super().__init__()
-        self.title("Library Management System")
+        self.title("Library Wanna-Be Management System")
         self.configure(background="whitesmoke")
         self.minsize(800, 600)
         
@@ -16,22 +16,21 @@ class LibraryApplication(tk.Tk):
         
         # File Menu
         file_menu = tk.Menu(menubar, tearoff=False)
-        file_menu.add_command(label='Exit Program', command=self.confirm_exit)
+        file_menu.add_command(label='Exit Program', command=self.ask_exit_confirmation)
         
         # Insert File Menu
         menubar.add_cascade(label='File', menu=file_menu)
         
-        #
+        # Closing the window
+        self.protocol("WM_DELETE_WINDOW", self.ask_exit_confirmation)
+        
+        # Start the main loop
         self.mainloop()
 
-    def confirm_exit(self) -> None:
-        """Ask the user to confirm exit from the program.
-
-        Returns:
-            None: _description_
-        """
-        result = messagebox.askquestion("Exit", "Are you sure you want to exit", icon='warning')
-        if result == "yes":
+    def ask_exit_confirmation(self) -> None:
+        """Ask the user to confirm exit from the program."""
+        
+        if messagebox.askokcancel("Exit", "Are you sure you want to exit?", icon='warning'):
             self.destroy()
         else:
             return None
